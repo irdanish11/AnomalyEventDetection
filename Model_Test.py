116c9f50372a92362d227772a799572dd80d6f2f
Junaid Muzaffar <62337545+junaid340@users.noreply.github.com>
2020-03-31 14:43:33 +0500

    Update Model_Test.py
    load_data function is changed. to load only canny images

--- Model_Test.py
+++ Model_Test.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Mar 27 17:27:23 2020
-
 @author: junaid
 """
 
 
 from keras.models import load_model
-from preprocessing import load_data
+from PreProcessing_V3 import load_data
 from sklearn.metrics import mean_squared_error as mse
 from tqdm import tqdm
+import numpy as np
 
 #Load the processed images and canny edged images
-x_test, lst = load_data(orig_frames = 'ProcessedImages', canned_frames = 'CannyImages')
+x_test, lst = load_data( canned_frames = 'test_CannyImages')
 
-model = load_model('Autoencoder_final.h5')
+model = load_model('Autoencoder_can.h5')
 #model.summary()
 
 result = []
@@ -22,13 +22,13 @@ losses = []
 classes = []
 #Loss threshold was selected on the basis training data: The maximum loss in the training data was 0.021
 #We made it .005 higher than the max value.
-loss_th = 0.026
+loss_th = 0.0020
 print('\nCalculating loss for the given inputs. \n')
 for i in tqdm(range(len(x_test))):
-    seq = x_test[i].reshape((1,8,128,128,1))
+    seq = x_test[i].reshape((1,4,128,128,1))
     pred = model.predict(seq)
-    pred = pred.reshape((8,128,128))
-    seq = seq.reshape((8,128,128))
+    pred = pred.reshape((4,128,128))
+    seq = seq.reshape((4,128,128))
     loss = []
     for i in range(len(seq)):
         l = mse(seq[i],pred[i])
@@ -41,4 +41,3 @@ for i in tqdm(range(len(x_test))):
     result.append(loss)
     
 max(losses)
-#is sy agy bta kia krna hy
